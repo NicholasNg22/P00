9311fff14d07e04158a5078f0c3d622ee9bcfc1e
21010468 <21010468@10.174.122.97>
2022-05-30 14:02:19 +0800

    first push to P00 repository in Github

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		//fail("Not yet implemented");
+		int a = 1234;
+		int b = 8765;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.add(a,  b);
+		
+		assertEquals (9999,actual);
+	}
+	
+	@Test
+	public final void testSubtract() {
+		int a = 9876;
+		int b = 4321;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.subtract(a,  b);
+		
+		assertEquals (5555,actual);
+	}
+
+}
